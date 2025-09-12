1f55fccb1e7370e1dde7e5bccdc087d43ae83088
makki9532 <makki9532@gmail.com>
2025-09-12 19:37:56 +0100

    changed host name

--- djangoapps/settings.py
+++ djangoapps/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ngrok.io', '92315c544661.ngrok-free.app']  # Add your ngrok subdomain here
+ALLOWED_HOSTS = ['*']  # Add your ngrok subdomain here
